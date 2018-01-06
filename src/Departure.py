228d17fdd8be2db6c609dc4e27c0669318ba06fe
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2018-01-06 21:34:03 +0100

    add persistence

--- src/Departure.py
+++ src/Departure.py
@@ -1,4 +1,5 @@
 import datetime
+import sqlite3
 
 
 class Departure:
@@ -23,14 +24,47 @@ class DepartureManager:
 
     def __init__(self):
         self.departures = {}
+        self.__prepareDatabase()
+
+    def __prepareDatabase(self):
+        self.sql_connection = sqlite3.connect('seeYaLater.db')
+        self.__runSQLCommand("""
+            CREATE TABLE IF NOT EXISTS departure (
+            id INT NOT NULL PRIMARY KEY,
+            line VARCHAR(4),
+            direction VARCHAR(42),
+            realTime DATETIME,
+            scheduledTime DATETIME
+            )
+            
+        """)
+
+    def __runSQLCommand(self, sql_command):
+        cursor = self.sql_connection.cursor()
+        cursor.execute(sql_command)
+        self.sql_connection.commit()
 
     def createOrUpdateDepartures(self, json):
-        for departureJson in json["Departures"]:
+        for departureJson in json['Departures']:
             departure = Departure(departureJson)
 
             if departure.id in self.departures:
-                print('update')
+                self.__persistDepartureUpdate(departure)
             else:
-                print('create')
+                self.__persistDepartureCreation(departure)
 
             self.departures[departure.id] = departure
+
+    def __persistDepartureCreation(self, departure: Departure):
+        command = """
+            INSERT INTO departure (id, line, direction, realTime, scheduledTime)
+            VALUES ({}, '{}', '{}', '{}', '{}')
+        """.format(departure.id, departure.line, departure.direction, departure.realTime,
+                   departure.scheduledTime)
+
+        self.__runSQLCommand(command)
+
+    def __persistDepartureUpdate(self, departure: Departure):
+        self.__runSQLCommand("""
+                    UPDATE departure SET scheduledTime = '{}' WHERE id = {}
+                """.format(departure.scheduledTime, departure.id))
