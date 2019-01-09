c96112d0d3dd508a40bde198411dd497aec0b62a
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2019-01-09 13:06:14 +0100

    TASK: kickstart frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <logo/>
+    Hello From NUXT!
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
