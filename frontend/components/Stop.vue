2748e5fb0ee5c203c5f410ac7c5091e72570cf1a
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2019-01-11 14:08:05 +0100

    TASK: show stop name on hover

--- frontend/components/Stop.vue
+++ frontend/components/Stop.vue
@@ -1,10 +1,24 @@
 <template>
-  <circle
-    :cx="y_coordinate"
-    :cy="x_coordinate"
-    r="30"
-    fill="red"
-  />
+  <g
+    class="stop"
+    @mouseenter="toggleName"
+    @mouseleave="toggleName"
+  >
+    <text
+      :x="x_coordinate + 100"
+      :y="y_coordinate + 60"
+      :class="{'stop_text--hidden': !nameIsVisible}"
+      class="stop__text"
+    >
+      {{ name }}
+    </text>
+    <circle
+      :cx="x_coordinate"
+      :cy="y_coordinate"
+      r="50"
+      class="stop__point"
+    />
+  </g>
 </template>
 
 <script>
@@ -21,6 +35,16 @@ export default {
     y_coordinate: {
       required: true,
       type: Number
+    }
+  },
+  data() {
+    return {
+      nameIsVisible: false
+    }
+  },
+  methods: {
+    toggleName() {
+      this.nameIsVisible = !this.nameIsVisible;
     }
   }
 };
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,10 +4,11 @@
       :viewBox="viewBox"
       height="100%"
       width="100%"
+      class="map"
     >
       <Stop
         v-for="stop in stops"
-        :key="stop.key"
+        :key="stop.id"
         :name="stop.name"
         :x_coordinate="stop.x_coordinate"
         :y_coordinate="stop.y_coordinate"
@@ -33,27 +34,27 @@ export default {
   },
   computed: {
     viewBox() {
-      return `${this.minX} ${this.minY} ${this.maxY} ${this.maxX} `
+      return `${this.minX - 1000} ${this.minY - 1000} ${this.maxX + 1000} ${this.maxY + 1000}`
     }
   },
   async created() {
     try {
       const response = await this.$axios.get('stops');
 
-      const x_coordinates = response.data.map(e => e.fields.x_coordinate);
+      const x_coordinates = response.data.map(e => e.x_coordinate);
       const minX = Math.min(...x_coordinates);
       const maxX = Math.max(...x_coordinates);
 
-      const y_coordinates = response.data.map(e => e.fields.y_coordinate);
+      const y_coordinates = response.data.map(e => e.y_coordinate);
       const minY = Math.min(...y_coordinates);
       const maxY = Math.max(...y_coordinates);
 
       this.stops = response.data.map(e => {
         return {
-          key: e.pk,
-          name: e.fields.name,
-          x_coordinate: e.fields.x_coordinate - minX,
-          y_coordinate: e.fields.y_coordinate - minY
+          id: e.id,
+          name: e.name,
+          x_coordinate: e.x_coordinate - minX,
+          y_coordinate: e.y_coordinate - minY
         }
       });
 
