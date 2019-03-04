5d864a24230eef6e400ae4e79bdeb4b5c1abd456
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2019-03-04 15:55:54 +0100

    TASK: draw tram lines as path

--- frontend/components/TramLine.vue
+++ frontend/components/TramLine.vue
@@ -2,13 +2,31 @@
   <g>
     <path
       :d="getLinePath()"
-      :fill="getColorForLine()"
+      :stroke="getColorForLine()"
+      class="tram-line"
+      stroke-width="20"
+      fill="transparent"
     />
   </g>
 </template>
 
 <script>
   import mapper from '~/utility/Mapper'
+
+  // get right side of the stop, 1 is up, 2 is right, 3 is down, 4 is left
+  function getSideOfStop(previousStop, currentStop) {
+    if (!previousStop) {
+      return 2;
+    }
+    const dx = currentStop.x - previousStop.x;
+    const dy = currentStop.y - previousStop.y;
+
+    if (Math.abs(dx) > Math.abs(dy)) {
+      return dx < 0 ? 2 : 4;
+    } else {
+      return dy < 0 ? 1 : 3;
+    }
+  }
 
   export default {
     props: {
@@ -32,6 +50,34 @@
         // first move to start point
         const startPoint = mapper.getStop(this.stops[0]);
         path += `M ${startPoint.x} ${startPoint.y}`;
+
+        let previousStop;
+        this.stops.forEach(stopId => {
+          const stop = mapper.getStop(stopId);
+          let coordinates = {x: stop.x, y: stop.y};
+
+          // offset to get the side
+          const sideOfStop = getSideOfStop(previousStop, stop);
+          const sideOffset = mapper.getOffsetForSide(stop.id, sideOfStop);
+          switch (sideOfStop) {
+            case 1:
+              coordinates.x += (sideOffset - 1) * 20;
+              break;
+            case 2:
+              coordinates.x += 100;
+              coordinates.y += ((sideOffset - 1) * 20);
+              break;
+            case 3:
+              coordinates.x += (sideOffset - 1) * 20;
+              coordinates.y += 100;
+              break;
+            case 4:
+              coordinates.y +=  ((sideOffset - 1) * 20);
+          }
+
+          path += ` L ${coordinates.x} ${coordinates.y}`;
+          previousStop = stop;
+        });
 
         return path;
       },
--- frontend/utility/Mapper.js
+++ frontend/utility/Mapper.js
@@ -13,12 +13,25 @@ class Mapper {
 
   constructor() {
     this.stops = {};
+    this.sideMapping = {};
+
     this.colorMap = {};
   }
 
   addStop = stop => this.stops[stop.id] = stop;
 
   getStop = stopId => this.stops[stopId];
+
+  getOffsetForSide = (stopId, sideIndex) => {
+    const keys = Object.keys(this.sideMapping);
+
+    if (keys.indexOf(stopId) === -1) {
+      this.sideMapping[stopId] = {1: 0, 2: 0, 3: 0, 4: 0};
+    }
+
+    this.sideMapping[stopId][sideIndex]++;
+    return this.sideMapping[stopId][sideIndex];
+  };
 
   getColorForLine(line) {
     const keys = Object.keys(this.colorMap);
